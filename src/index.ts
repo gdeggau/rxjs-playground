996c04a57af3c1eb99cd802552f919bcd1f8b588
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-10-26 09:37:28 -0300

    order

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ const observable$ = new Observable<string>((subscriber) => {
   console.log("Observable Executed");
   subscriber.next("Alice");
   subscriber.next("Ben");
-  setTimeout(() => {
-    subscriber.next("Charlie");
-  }, 2000);
 
   setTimeout(() => {
     subscriber.error(new Error("Failure"));
+  }, 2000);
+
+  setTimeout(() => {
+    subscriber.next("Charlie");
+    subscriber.complete();
   }, 4000);
 
   return () => {
@@ -21,4 +23,5 @@ console.log("Before subscribe");
 observable$.subscribe({
   next: (value) => console.log(value),
   error: (err) => console.log(err.message),
+  complete: () => console.log("Completed"),
 });
