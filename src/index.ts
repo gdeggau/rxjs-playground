ca3194f6053621a8aed453ff9691db30afc0bc61
Gabriel Deggau Schmidt <gabriel_d_schmidt@hotmail.com>
2022-10-27 09:27:35 -0300

    creation function - from

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,19 @@
-import { Observable, of } from "rxjs";
+import { from, Observable, of } from "rxjs";
 
-ourOwnOf("Alice", "Ben", "Charlie").subscribe({
-  next: (value) => console.log(value),
-  complete: () => console.log("Completed"),
-});
-
-// const names$ = new Observable<string>((subscriber) => {
-//   subscriber.next("Alice");
-//   subscriber.next("Charlie");
-//   subscriber.next("Ben");
-//   subscriber.complete();
-// });
-
-// names$.subscribe({
+// from(["Alice", "Ben", "Charlie"]).subscribe({
 //   next: (value) => console.log(value),
 //   complete: () => console.log("Completed"),
 // });
 
-function ourOwnOf(...args: string[]): Observable<string> {
-  return new Observable<string>((subscriber) => {
-    args.forEach((arg) => {
-      subscriber.next(arg);
-    });
-    subscriber.complete();
-  });
-}
+const somePromise = new Promise((resolve, reject) => {
+  resolve("Resolved");
+  // reject("Rejected");
+});
+
+const observableFromPromise$ = from(somePromise);
+
+observableFromPromise$.subscribe({
+  next: (value) => console.log(value),
+  error: (err) => console.log("Error: ", err),
+  complete: () => console.log("Completed"),
+});
